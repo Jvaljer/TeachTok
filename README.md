a7fab57a4a16a56afc0906300341ed23c4252492
Jvaljer <abbel.haol@gmail.com>
2024-09-18 13:56:02 +0200

    sorta forking from gitlab when I did the TER project

---
+++ README.md
@@ -0,0 +1,19 @@
+TeachTOK is a collaborative interactive machine teaching prototype of [Marcelle](https://marcelle.dev) toolkit for image classification task. 
+
+> link to the paper: to be published.
+
+> link to the demo: https://marcelle.lisn.upsaclay.fr/teachtok/ 
+
+## Available Scripts
+
+### npm run dev
+
+Runs the app in the development mode.
+Open http://localhost:5173 to view it in the browser.
+
+The page will reload if you make edits.
+
+### npm run build
+
+Builds a static copy of your site to the `dist/` folder.
+Your app is ready to be deployed!
---
+++ src/routes/app/help/+page.svelte
@@ -0,0 +1,25 @@
+<script>
+  import { base } from '$app/paths';
+  import { logEvent } from '$lib/marcelle/log';
+
+  import { onMount } from 'svelte';
+
+  onMount(() => {
+    logEvent('navigate', 'help');
+  });
+</script>
+
+<svelte:head>
+  <title>Help</title>
+</svelte:head>
+
+<div class="flex justify-center w-full">
+  <!-- svelte-ignore a11y-media-has-caption -->
+  <video controls class="max-w-4xl w-full">
+    <source src="{base}/tutorial.mp4" type="video/mp4" />
+    Sorry, your browser doesn't support embedded videos.
+  </video>
+</div>
+
+<style>
+</style>
